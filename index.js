4e640559640032b8786387af03dd481fd27de7ba
Etta Baker <etta.r.baker@gmail.com>
2022-03-12 23:37:01 -0500

    Used express for HTTP requests

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Requiring express and morgan
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();
+
 // Create an array of top movies
 let topMovies = [
     {
@@ -52,3 +58,32 @@ let topMovies = [
     },
 ];
 
+// Middleware function to use morgan for loggin
+app.use(morgan('common'));
+// Middleware function for using public folder for routing to static files
+app.use(express.static('public'));
+// Error handling function
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+// HTTP GET Requests
+app.get('/', (req, res) => {
+    res.send('Welcome to my Movie Database!');
+});
+
+// Return documetantion page when navigating to the /documentation URL
+// app.get('/documentation.html', (req, res) => {
+    
+// });
+
+// Return my list of top ten movies when navigating to the /movies URL
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+// Listen for requests
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
