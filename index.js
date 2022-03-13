df9dfa58e116ede5458d63256536beb8e6b9ae76
Etta Baker <etta.r.baker@gmail.com>
2022-03-12 23:35:55 -0500

    Added my top ten movies as an array

---
+++ index.js
@@ -0,0 +1,54 @@
+// Create an array of top movies
+let topMovies = [
+    {
+        title: 'Big Hero 6',
+        director: 'Don Hall & Chris Williams',
+        release_date: '2014'
+    },
+    {
+        title: 'Easy A',
+        director: 'Will Gluck',
+        release_date: '2010'
+    },
+    {
+        title: 'JoJo Rabbit',
+        director: 'Taika Waititi',
+        release_date: '2019'
+    },
+    {
+        title: 'Brave',
+        director: 'Mark Andrews, Brenda Chapman, & Steve Purcell',
+        release_date: '2012'
+    },
+    {
+        title: 'Matilda',
+        director: 'Danny DeVito',
+        release_date: '1996'
+    },
+    {
+        title: 'Martian Child',
+        director: 'Menno Meyjes',
+        release_date: '2007'
+    },
+    {
+        title: '10 Things I Hate About You',
+        director: 'Gil Junger',
+        release_date: '1999'
+    },
+    {
+        title: 'The Sixth Sense',
+        director: 'M. Night Shyamalan',
+        release_date: '1999'
+    },
+    {
+        title: 'My Girl',
+        director: 'Howard Zieff',
+        release_date: '1991'
+    },
+    {
+        title: 'Elf',
+        director: 'Jon Favreau',
+        release_date: '2003'
+    },
+];
+
