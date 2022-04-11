63faaf610a849152fe9106cde5114ca97c951337
Etta Baker <etta.r.baker@gmail.com>
2022-04-10 21:29:26 -0400

    Updated validation in update user info endpoint

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
     Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
         {
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday,
         }
