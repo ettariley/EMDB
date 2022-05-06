9f59969b9646f7f0d648dbbaa5621c1e4fb361c0
Etta Baker <etta.r.baker@gmail.com>
2022-05-05 23:51:10 -0400

    Update get user info

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('/director/:Name', passport.authenticate('jwt', { session: false }), (re
 });
 
 // READ user info
-app.get('/userdata/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOne({ Username: req.params.Username })
         .then((user) => {
             res.json(user);
