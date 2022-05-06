485072e29658f8cf1c55cc88c400ce52b8874f56
Etta Baker <etta.r.baker@gmail.com>
2022-05-05 21:55:44 -0400

    Added get user info route

--- index.js
+++ index.js
@@ -81,6 +81,17 @@ app.get('/director/:Name', passport.authenticate('jwt', { session: false }), (re
         });
 });
 
+// READ user info
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+
 // CREATE user
 app.post('/users', 
      // Validation logic here for request
